8fcb61f4b6860ef340ccaeca91084d71c99157b8
Anand <aanand.raj.patna@gmail.com>
2023-07-12 00:28:55 +0530

    Initial commit of project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+const skills = [
+  {
+    skill: 'HTML+CSS',
+    level: 'advanced',
+    color: '#2662EA',
+  },
+  {
+    skill: 'JavaScript',
+    level: 'advanced',
+    color: '#EFD81D',
+  },
+  {
+    skill: 'Web Design',
+    level: 'advanced',
+    color: '#C3DCAF',
+  },
+  {
+    skill: 'Git and GitHub',
+    level: 'intermediate',
+    color: '#E84F33',
+  },
+  {
+    skill: 'React',
+    level: 'advanced',
+    color: '#60DAFB',
+  },
+  {
+    skill: 'Python',
+    level: 'beginner',
+    color: '#FF3B00',
+  },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Image src={'profile.png'} />
+      <Desc />
+      <Skills skills={skills} />
     </div>
   );
 }
 
-export default App;
+function Image({ src }) {
+  return (
+    <div>
+      <img src={src} alt="profile user" />
+    </div>
+  );
+}
+
+function Desc() {
+  return (
+    <div className="desc">
+      <h1>Anand</h1>
+      <p>
+        Hi,I am highly enthusiast web developer who loves to work in Work
+        Development
+      </p>
+    </div>
+  );
+}
+
+function Skills({ skills }) {
+  return (
+    <div className="skills">
+      {skills.map((skill, i) => (
+        <Skill
+          skill={skill.skill}
+          level={skill.level}
+          color={skill.color}
+          key={i}
+        />
+      ))}
+    </div>
+  );
+}
+function Skill({ skill, level, color }) {
+  return (
+    <div className="skill">
+      <button style={{ backgroundColor: color }}>
+        {skill}
+        <span>
+          {level === 'advanced' && '💪'}
+          {level === 'intermediate' && '👍'}
+          {level === 'beginner' && '👶'}
+        </span>
+      </button>
+    </div>
+  );
+}
